b6697f7c10450095739f8323bc72124650516953
Contour gara <the.rocker106@outlook.jp>
2023-07-21 21:01:02 +0900

    1章完了

--- src/main/java/part1/Caluculator.java
+++ src/main/java/part1/Caluculator.java
@@ -1,11 +1,13 @@
 package part1;
 
 public class Caluculator {
-    public int multiply(int x, int y){
+    public int multiply(int x, int y) {
         return x * y;
     }
-    public int divide(int x, int y){
-        return x / y;
+
+    public float divide(int x, int y) {
+        if (y == 0) throw new IllegalArgumentException("divide by zero");
+        return (float) x / (float) y;
     }
 
 }
--- src/test/java/part1/CaluclatorTest.java
+++ src/test/java/part1/CaluclatorTest.java
@@ -6,11 +6,33 @@ import org.junit.Test;
 
 public class CaluclatorTest {
     @Test
-    public void multiplyで乗算結果が取得できる() {
+    public void multiplyで3と4の乗算結果が取得できる() {
         Caluculator caluculator = new Caluculator();
         int expected = 12;
-        int actual = caluculator.multiply(2, 6);
+        int actual = caluculator.multiply(3, 4);
         assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void multiplyで5と7の乗算結果が取得できる() {
+        Caluculator caluculator = new Caluculator();
+        int expected = 35;
+        int actual = caluculator.multiply(5, 7);
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void divideで3と2の乗算結果が取得できる() {
+        Caluculator caluculator = new Caluculator();
+        float expected = 1.5f;
+        float actual = caluculator.divide(3, 2);
+        assertThat(actual, is(expected));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void divideで5と0のときIllegalArgumentExceptionを送出する() {
+        Caluculator caluculator = new Caluculator();
+        caluculator.divide(5, 0);
     }
 
 }
